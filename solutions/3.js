463a0fbbcd76dc51ff36e05a631fa84240fe673c
BarthesSimpson <d.s.frenk@gmail.com>
2017-08-29 22:46:24 -0500

    solved problem 3 and added some comments

---
+++ solutions/3.js
@@ -0,0 +1,22 @@
+/*
+The prime factors of 13195 are 5, 7, 13 and 29.
+What is the largest prime factor of the number 600851475143 ?
+*/
+
+// LPF of a number must be either the number itself or at most the sqrt of the number
+// So, here is a simple recursive approach
+function largestPrimeFactor(val, divisor = 2) { 
+    
+    // count up to the sqrt of the search value until we find a divisor
+    while ((val % divisor) !== 0 && divisor**2 <= val) {
+        divisor++
+    }
+
+    // if we already exceeded the square root of the search value, we have found the LPF
+    // otherwise, divide by the divisor and recurse
+    return divisor**2 <= val
+        ? largestPrimeFactor(val / divisor, divisor)
+        : val
+}
+
+console.log(largestPrimeFactor(600851475143))
