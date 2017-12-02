a236e8dc73887b99238e4f18ea378563c1b6be46
BarthesSimpson <d.s.frenk@gmail.com>
2017-12-01 21:43:23 -0600

    cleanup

--- solutions/5.js
+++ solutions/5.js
@@ -6,15 +6,22 @@ What is the smallest positive number that is evenly divisible by all of the numb
 const getPrimes = require('../helpers/getPrimes')
 
 // Do prime factorization then multiply the result
+/**
+ * Find the smallest positive number that is divisible by all
+ * numbers in a specified range
+ * @param {number} ceil - ceiling of the range (inclusive)
+ * @param {number} floor - floor of the range (inclusive)
+ * @returns {number}
+ */
 function smallestMultiple(ceil, floor = 1) {
-    const primes = getPrimes(ceil, floor)
-    return primes.reduce((l, r) => {
-        let power = 1
-        while (power < ceil && r ** power < ceil) {
-            power++
-        }
-        return l * r ** --power
-    }, 1)
+  const primes = getPrimes(ceil, floor)
+  return primes.reduce((l, r) => {
+    let power = 1
+    while (power < ceil && r ** power < ceil) {
+      power++
+    }
+    return l * r ** --power
+  }, 1)
 }
 
 console.log(smallestMultiple(20))
