60f1f3b85614c834f2904f9c1ca10f59a97e5636
BarthesSimpson <d.s.frenk@gmail.com>
2018-01-10 23:11:37 -0600

    solved problem 30

--- solutions/30.js
+++ solutions/30.js
@@ -1,12 +1,31 @@
 /*
 Surprisingly there are only three numbers that can be written as the sum of fourth powers of their digits:
 
-1634 = 14 + 64 + 34 + 44
-8208 = 84 + 24 + 04 + 84
-9474 = 94 + 44 + 74 + 44
-As 1 = 14 is not a sum it is not included.
+1634 = 1^4 + 6^4 + 3^4 + 4^4
+8208 = 8^4 + 2^4 + 0^4 + 8^4
+9474 = 9^4 + 4^4 + 7^4 + 4^4
+As 1 = 1^4 is not a sum it is not included.
 
 The sum of these numbers is 1634 + 8208 + 9474 = 19316.
 
 Find the sum of all the numbers that can be written as the sum of fifth powers of their digits. 
 */
+
+/**
+ * Find the sum of all numbers that can be written as the sum
+ * of fifth powers of their digits
+ */
+
+function sumOfPentagonalNumbers() {
+  let sum = 0
+  for (let i = 9; i < 10 ** 6; i++) {
+    const sumFifth = Array.from(i.toString())
+      .map(i => i ** 5)
+      .reduce((a, b) => a + b, 0)
+    if (sumFifth === i) {
+      sum += i
+    }
+  }
+  return sum
+}
+console.log(sumOfPentagonalNumbers())
